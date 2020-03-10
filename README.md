732724be193a2c539fac2dab8f834102597fe00e
rsizem2 <sizemore.robert.k@gmail.com>
2020-03-10 17:17:39 -0400

    added comments

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My solution scripts for various exercises in Trefethen &amp; Bau's 'Numerical Li
 
 # Why?
 
-Althought the book is written for use with MATLAB, I wanted to do the solutions using Python libraries. This is just an excuse to keep my linear algebra skills fresh and practice using python libraries.
+Although the book is written for use with MATLAB, I wanted to do the solutions using Python libraries. This is just an excuse to keep my linear algebra skills fresh and practice using python libraries.
--- chap4/ex3.py
+++ chap4/ex3.py
@@ -4,13 +4,13 @@ from matplotlib import patches
 import matplotlib.pyplot as plt
 
 
-# Define matrix to compute SVD of
+# define 2x2 matrix
 X = np.array([[5,2],
               [2,3]])
 
 U,S,V = np.linalg.svd(X)
 
-# Pre-image of A, column vectors of V
+# right singular vectors
 v1 = patches.Arrow(x = 0, y = 0, 
                    dx = V[0][0], 
                    dy = V[1][0],
@@ -22,10 +22,10 @@ v2 = patches.Arrow(x = 0, y = 0,
                    width = 0.2,
                    color = "black")
 
-# Unit circle (technically an ellipse)
+# unit circle (technically an ellipse)
 circ = patches.Ellipse((0,0), 2, 2)
 
-# Semi-axes of the ellipse, scaled columns of U
+# left singular vectors, semi-axes of the image ellipse
 u1 = patches.Arrow(x = 0, y = 0, 
                    dx = S[0]*U[0][0], 
                    dy = S[0]*U[1][0],
@@ -37,17 +37,20 @@ u2 = patches.Arrow(x = 0, y = 0,
                    width = 0.2,
                    color = "black")
 
-# Image of the unit circle 
+# angle of rotation of the ellipse
 if U[1][0] < 0:
     ang = 2*np.pi - np.arccos(U[0][0])
 else:
     ang = np.arccos(U[0][0])
-ellps = patches.Ellipse((0,0), 2*S[0], 2*S[1], angle = (ang)*180/np.pi)
 
-# Subplots
+# ellipse representing the image of the unit circle    
+ellps = patches.Ellipse((0,0), 2*S[0], 2*S[1], angle = ang*180/np.pi)
+
+
+
 fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2)
 
-# Plot1
+# left subplot
 ax1.add_patch(circ)
 ax1.add_patch(v1)
 ax1.add_patch(v2)
@@ -57,7 +60,7 @@ ax1.set(xlim = (-1.5,1.5), ylim = (-1.5,1.5),
 ax1.set_title("Right Singular Vectors")
 
 
-# Plot 2
+# right subplot
 ax2.add_patch(ellps)
 ax2.add_patch(u1)
 ax2.add_patch(u2)
